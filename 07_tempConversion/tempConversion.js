750d4d7cb1040c31d0b6f1c04fe5cb72ab1e8d06
michalwachowicz <wachowiczmichal1@gmail.com>
2024-06-28 15:35:11 +0200

    Solve the 07_tempConversion problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
-};
+const roundToOneDecimal = (value) => Math.round(value * 10) / 10;
 
-const convertToFahrenheit = function() {
-};
+// Formula: (Temperature in F - 32) * 5/9
+const convertToCelsius = (temp) => roundToOneDecimal((temp - 32) * (5 / 9));
+
+// Formula: (9/5 * temperature in C) + 32
+const convertToFahrenheit = (temp) => roundToOneDecimal((9 / 5) * temp + 32);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
