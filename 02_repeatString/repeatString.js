51e03973166aced170567b61f58cdf1a85b3f68b
michalwachowicz <wachowiczmichal1@gmail.com>
2024-06-28 14:28:32 +0200

    Solve the 02_repeatString problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = (text, n) => {
+  if (n < 0) return "ERROR";
 
+  let repeatedString = "";
+
+  for (let i = 0; i < n; i++) {
+    repeatedString = repeatedString.concat(text);
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
