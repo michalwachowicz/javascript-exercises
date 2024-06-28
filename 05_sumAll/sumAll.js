97dae3c09834814ecf3e256d9b0c5dcf2073b759
michalwachowicz <wachowiczmichal1@gmail.com>
2024-06-28 15:10:31 +0200

    Solve the 05_sumAll problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const isPositiveNumber = (num) => typeof num == "number" && num >= 0;
 
+const sumAll = (a, b) => {
+  if (!isPositiveNumber(a) || !isPositiveNumber(b)) return "ERROR";
+
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
