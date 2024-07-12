e6533c97a66d968ebdcccc89e884debcf3f06fe8
michalwachowicz <wachowiczmichal1@gmail.com>
2024-07-12 11:21:10 +0200

    Solve the 09_palindromes problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = (str) => {
+  const formatted = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return formatted == formatted.split("").reverse().join("");
 };
 
 // Do not edit below this line
