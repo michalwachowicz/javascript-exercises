9864bce37290ef46d0a5269b8323e5dd12d96f34
michalwachowicz <wachowiczmichal1@gmail.com>
2024-07-13 13:24:31 +0200

    Solve the 09_fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = (val) => {
+  const num = parseInt(val);
+  if (isNaN(num) || num < 0) return "OOPS";
 
+  return Array.from({ length: num + 1 }).reduce((arr, _, i) => {
+    arr.push(i <= 1 ? i : arr[i - 2] + arr[i - 1]);
+    return arr;
+  }, [])[num];
 };
 
 // Do not edit below this line
