0562008cac3d3503908548a11858c68a34d1c0d9
michalwachowicz <wachowiczmichal1@gmail.com>
2024-07-13 14:08:05 +0200

    Solve the 12_findTheOldest problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+  const currentYear = new Date().getFullYear();
 
+  return people
+    .map((person) => {
+      const yearOfDeath = person.yearOfDeath || currentYear;
+      const age = !person.yearOfBirth ? 0 : yearOfDeath - person.yearOfBirth;
+
+      return {
+        ...person,
+        yearOfDeath,
+        age,
+      };
+    })
+    .sort((a, b) => (a.age > b.age ? -1 : 1))[0];
 };
 
 // Do not edit below this line
